ca2bb3f0caffe057f05dcfac3429bcf4a4a27af5
Marc Robison <marcrobison@gmail.com>
2023-11-11 00:49:22 +0100

    new password generation implimentation questions

--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@
 #include <time.h>
 #include <assert.h>
 
+#define MAX_CHAR_TYPE_COMBINATIONS 4
+
 #define NUM_OF_LETTERS_IN_ALPHABET 26
 #define	UPPERCASE_OFFSET 0x41
 #define	LOWERCASE_OFFSET 0x61
@@ -25,15 +27,39 @@
 void InitSpecialCharArr(char *array);
 void FillSpecialCharArr(char *array, int *index, int start, int end);
 
+/* 
+ * Formats and stores Character combination into char as a bitmask.
+ * Arguments:
+ * 	- `combinations`: pointer to string that needs parsing
+ * - `num_combinations`: the size of the string
+ *   Returns:
+ *   	An int revealing the chosen options
+ *
+ *   	Bit num | Option
+ *   	------- | -------------
+ *   	1	|  Uppercase 
+ *   	2	|  Lowercase
+ *   	3	|  Number
+ *   	4	|  Special char	
+ *
+ *   	for example:
+ *   	0b1000 means user wants an Uppercase password
+ *   	or
+ *	0b0101 means user wants both Lowercase and Special characters
+ */
+int ParseCharCombinations(char *combinations, int num_combinations);
+
 /*
  * Generates a random string of a given type.
  * Arguments:
- * 	- `type`: a character to indicate the group of characters allowed in the string.
+ * 	- `types`: a int to indicate the group of characters allowed in the string
+ * 	see ParseCharCombinations() for a detailed explenation
  *  - `size`: the length in characters of the requested string.
  * Returns:
  * 	A string of length `size` with only characters of type `type`. Returns `NULL` if `malloc` fails.
 */
-char *GeneratePass(char type, size_t size);
+
+char *GeneratePass(int types, size_t size);
 
 /*
  * Prints the allowed arguments for the program.
@@ -42,66 +68,86 @@ void ShowArguments();
 
 int main(int argc, char** argv) 
 {
-	switch (argc) 
+	int num_combinations = 0;
+	num_combinations = strlen(argv[1]);
+	if(num_combinations <= MAX_CHAR_TYPE_COMBINATIONS) // 4 or less char combinations requested by user
 	{
-
-		case 1:
-			fprintf(stderr, "No arguments specified");
-			ShowArguments();
-			return 1; // No need to break.
-		case 2:
-			fprintf(stderr, "Missing one argument");
-			ShowArguments();
-			return 1;
-		default:
-			{
-			int size = -1;
-			if( IS_NUMBER(*argv[2]) )
-			{
-				size = atoi(argv[2]);
-				char *pass = GeneratePass(*argv[1], size);
-				if (pass != NULL) 
-				{
-					printf("Generated password: %s\n", pass);
-					free(pass);
-				}
-				return 0; // No need to break.
-			}
-			else
-			{
-				fprintf(stderr, "Invalid Number of characters\n");
+		switch (argc) 
+		{
+			case 1:
+				fprintf(stderr, "No arguments specified");
+				ShowArguments();
+				return 1; // No need to break.
+			case 2:
+				fprintf(stderr, "Missing one argument");
+				ShowArguments();
 				return 1;
-			}
+			default:
+				{
+				int size = -1;
+				if( IS_NUMBER(*argv[2]) )
+				{
+					size = atoi(argv[2]);
 
-			}
+					int combinations = ParseCharCombinations(argv[1], num_combinations);
+					char *pass = GeneratePass(combinations, size);
+					if (pass != NULL) 
+					{
+						#ifdef WRITE_TO_FILE
+						
+						#endif
+						printf("Generated password: %s\n", pass);
+						free(pass);
+					}
+					return 0; // No need to break.
+				}
+				else
+				{
+					fprintf(stderr, "Invalid Number of characters\n");
+					return 1;
+				}
+
+				}
+		}
+	}
+	else
+	{
+		fprintf(stderr, "Too many combinations specified");
+		ShowArguments();
 	}
 }
 
-char *GeneratePass(char char_type, size_t size)
+char *GeneratePass(int char_types, size_t size)
 {
-
+	printf("%d",char_types);
 	char specialchar[25];
 	char *pass = (char*)malloc(size);
 	if(pass != NULL)
 	{
 		srand(time(NULL));
 
-		switch(char_type)
+		// generate random number from 0 to 3
+		// then left shift by that number
+		// random number
+		//
+		// Doesn't quite work for all cases
+		
+		switch(char_types)
 		{
 			// randomly choose characters and fill array
-			case 'u':
+			case 0b1000:
 				for(int count = 0; count < size; count++)
 					 pass[count] = rand()%NUM_OF_LETTERS_IN_ALPHABET + UPPERCASE_OFFSET;
 				break;
-			case 'l':
+			case 0b0100:
 				for(int count = 0; count < size; count++)
 					 pass[count] = rand()%NUM_OF_LETTERS_IN_ALPHABET + LOWERCASE_OFFSET;
 				break;
-			case 'n':
+			case 0b0010:
 				for(int count = 0; count < size; count++)
 					 pass[count] = rand()%10 + NUMBERS_OFFSET;
 				break;
-			case 's':
+			case 0b0001:
 				InitSpecialCharArr(specialchar);
 				for(int count = 0; count < size; count++)
 					pass[count] = specialchar[ rand()%25 ];
@@ -134,6 +180,30 @@ void FillSpecialCharArr(char *array, int *index, int start, int end)
 		array[*index] = count;	
 		(*index)++;
 	}
+}
+
+int ParseCharCombinations(char *arguments, int num_combinations)
+{
+	int combination = 0;
+	for(int count = 0; count < num_combinations; count++)
+	{
+		switch (arguments[count])
+		{
+			case 'u':
+				combination |= 0b1000;
+				break;
+			case 'l':
+				combination |= 0b0100;
+				break;
+			case 'n':
+				combination |= 0b0010;
+				break;
+			case 's':
+				combination |= 0b0001;
+				break;
+		}
+	}
+	return combination;
 }
 
 void ShowArguments(void)
